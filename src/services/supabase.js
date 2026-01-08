30d24975d8c475c29c712f5c839fc7bb0f04bead
Jackson McCaslin <jmccaslin@MacBook-Air-1039.local>
2026-01-08 13:35:30 +0100

    Updating unnecessary env variable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_DB_URL;
+export const supabaseUrl = "https://mysbrkriwelivdpkknbq.supabase.co";
 const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
