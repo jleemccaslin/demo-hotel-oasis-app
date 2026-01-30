5c27cbced3824414bcbb1a6c130a6a4412dc3b5b
Jackson McCaslin <jmccaslin@MacBook-Air-1039.local>
2026-01-30 11:43:17 +0100

    Initial Typescript install

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App";
 import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./ui/ErrorFallback.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
@@ -12,5 +12,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     >
       <App />
     </ErrorBoundary>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
